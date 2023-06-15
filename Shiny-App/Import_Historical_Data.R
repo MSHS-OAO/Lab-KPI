4fbd6c849ee93d1050b98b8f86d70340f7e00849
kateenevin <kate.nevin@mssm.edu>
2023-06-15 15:23:01 -0400

    Add conditional formatting for never events and start working on code for kables.

--- Shiny-App/Import_Historical_Data.R
+++ Shiny-App/Import_Historical_Data.R
@@ -25,7 +25,7 @@ cp_submitted_daily_summary <- cp_daily_repo %>%
 
 
 # Operations and Quality Indicators -------
-ops_qlty_date <- Sys.Date() - 2
+ops_qlty_date <- Sys.Date() - 3
 
 ops_qlty_data <- read_excel(
   paste0(user_directory,
@@ -54,6 +54,15 @@ ops_qlty_responses_today <- ops_qlty_responses %>%
            (CompletionDate == ops_qlty_date - 1 &
               CompletionHour >= 17))
 
+# Ensure all facilities are in dataset even if there weren't any responses
+ops_qlty_responses_today <- left_join(ops_indicators_facility_df,
+                                      ops_qlty_responses_today,
+                                      by = c("Facility" = "Facility"))
+
+ops_qlty_responses_today <- ops_qlty_responses_today %>%
+  select(-Facility) %>%
+  rename("Facility" = FacilitySimple)
+
 ops_indicators_responses <- ops_qlty_responses_today %>%
   select(-ID, -StartTime, -CompletionTime, -Email, -Name,
          -NeverEvents, -NeverEventsComments, -GoodCatch,
@@ -81,7 +90,7 @@ ops_qlty_formatting <- function(x) {
   x <- x %>%
     mutate(Status = ifelse(is.na(Status),
                            cell_spec(Status, "html", color = "lightgray"),
-                           ifelse(Status %in% c("Safe", Status == "None"),
+                           ifelse(Status %in% c("Safe", "None"),
                                   cell_spec(Status, "html", color = "green"),
                                   ifelse(Status %in% c("Not Safe", "Present"),
                                          cell_spec(Status, "html", color = "red"),
@@ -97,16 +106,6 @@ ops_indicators_responses <- ops_indicators_responses %>%
               values_from = "Status") %>%
   relocate(Comments, .after = last_col())
 
-ops_indicators_sites_df <- data.frame("Facility" = ops_indicators_sites)
-
-ops_indicators_responses <- left_join(ops_indicators_facility_df,
-                                      ops_indicators_responses,
-                                      by = c("Facility" = "Facility"))
-
-ops_indicators_responses <- ops_indicators_responses %>%
-  select(-Facility) %>%
-  rename("Facility" = "FacilitySimple")
-
 ops_indicators_status <- ops_indicators_responses %>%
   select(-Comments)
 
@@ -117,3 +116,95 @@ ops_indicators_comments <- ops_indicators_responses %>%
                            "No Issues Reported (Safe)", Comments)
   )
 
+
+# Never Events ------
+never_event_options <- c("Specimen(s) Lost",
+                         "QNS - specimen that cannot be recollected",
+                         "Treatment based on mislabeled/misidentified specimen",
+                         "Treatment based on false positive/false negative results")
+
+never_events_responses$NeverEvents <- c("None",
+                                        paste0(never_event_options[1],
+                                               ";",
+                                               never_event_options[3],
+                                               ";"),
+                                        paste0(never_event_options[2], ";"),
+                                        "None",
+                                        "None",
+                                        "None",
+                                        "None",
+                                        "None",
+                                        "None",
+                                        "None",
+                                        "None")
+never_events_responses <- never_events_responses %>%
+  mutate(SpecimenLost = ifelse(str_detect(NeverEvents, "Specimen\\(s\\) Lost"),
+                               "Present", "None"),
+         QNS = ifelse(str_detect(NeverEvents, "QNS"), "Present", "None"),
+         Mislabeled = ifelse(str_detect(NeverEvents,
+                                        "Treatment based on mislabeled"),
+                             "Present", "None"),
+         FalsePositive = ifelse(str_detect(NeverEvents,
+                                           "Treatment based on false positive"),
+                                "Present", "None"),
+         NeverEventsComments = ifelse(is.na(NeverEventsComments) |
+                                        toupper(NeverEventsComments) %in%
+                                        c("NONE", "N/A", "NA"),
+                                      "No Issues Reported",
+                                      NeverEventsComments)
+  ) %>%
+  relocate(NeverEventsComments, .after = last_col()) %>%
+  select(-NeverEvents)
+
+
+
+never_events_status <- never_events_responses %>%
+  select(-NeverEventsComments) %>%
+  pivot_longer(cols = !Facility,
+               names_to = "Metric",
+               values_to = "Status")
+
+never_events_status <- ops_qlty_formatting(never_events_status)
+
+never_events_status <- never_events_status %>%
+  pivot_wider(names_from = "Metric",
+              values_from = "Status")
+
+never_events_comments <- never_events_responses %>%
+  select(Facility, NeverEventsComments)
+
+good_catch_responses <- good_catch_responses %>%
+  mutate(GoodCatch = ifelse(is.na(GoodCatch) |
+                              toupper(GoodCatch) %in%
+                              c("NONE", "N/A", "NA", 0),
+                            "No Issues Reported", GoodCatch)
+  )
+
+# Kables
+kable(ops_indicators_status, escape = FALSE, align = "c",
+      col.names = c("Facility", "Lab Corp Consumables",
+                    "Vendor Services", "Environment", "Equipment",
+                    "IT", "Service Change", "Acute Volume Increase",
+                    "Staffing")) %>%
+  kable_styling(bootstrap_options = "hover", full_width = FALSE,
+                 position = "center", row_label_position = "c",
+                 font_size = 11) %>%
+  row_spec(row = 0, font_size = 13)
+
+kable(ops_indicators_comments, escape = F, align = "c",
+      col.names = c("Facility", "Comments if At Risk or Not Safe")) %>%
+  kable_styling(bootstrap_options = "hover", full_width = TRUE,
+                position = "center", row_label_position = "c",
+                font_size = 11) %>%
+  row_spec(row = 0, font_size = 13)
+
+kable(never_events_status, escape = F, align = "c",
+      col.names = c("Facility",
+                    "Specimen(s) Lost",
+                    "QNS - specimen that cannot be recollected",
+                    "Treatment based on mislabeled/misidentified specimen",
+                    "Treatment based on false positive/false negative results")) %>%
+  kable_styling(bootstrap_options = "hover", full_width = FALSE,
+                position = "center", row_label_position = "c",
+                font_size = 11) %>%
+  row_spec(row = 0, font_size = 13)
