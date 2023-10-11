3ed3f02b1013ef168a507d76852642bc7a140c3c
kateenevin <kate.nevin@mssm.edu>
2023-10-11 12:25:11 -0400

    Kate testing old files.

--- Lab KPI Daily Dashboard/Lab_KPI_Prelim_Run_Render.R
+++ Lab KPI Daily Dashboard/Lab_KPI_Prelim_Run_Render.R
@@ -4,16 +4,16 @@
 rm(list = ls())
 
 # Determine directory
-# if ("Presidents" %in% list.files("J://")) {
-#   user_directory <- paste0("J:/Presidents/HSPI-PM/",
-#                            "Operations Analytics and Optimization/Projects/",
-#                            "Service Lines/Lab Kpi/Data")
-# } else {
-#   user_directory <- paste0("J:/deans/Presidents/HSPI-PM/",
-#                            "Operations Analytics and Optimization/Projects/",
-#                            "Service Lines/Lab Kpi/Data")
-# }
-user_directory <- "/Pathology/"
+if ("Presidents" %in% list.files("J://")) {
+  user_directory <- paste0("J:/Presidents/HSPI-PM/",
+                           "Operations Analytics and Optimization/Projects/",
+                           "Service Lines/Lab Kpi/Data")
+} else {
+  user_directory <- paste0("J:/deans/Presidents/HSPI-PM/",
+                           "Operations Analytics and Optimization/Projects/",
+                           "Service Lines/Lab Kpi/Data")
+}
+# user_directory <- "/Pathology/"
 
 # Render markdown file with dashboard code and save with today's date
 rmarkdown::render(paste0("Lab KPI Daily Dashboard/",
